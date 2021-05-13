9b8be85c74fdc9dfba0a29b5fc22d97c7bcf912b
prhckspc <localhost@localdomain.local>
2021-05-13 16:26:28 +0200

    Modified code(deleted some unnecessary code) with comments

--- Pyweb.py
+++ Pyweb.py
@@ -1,9 +1,11 @@
+#Import requirements
 import sys
 from PySide2 import QtCore
 from PySide2.QtCore import Qt, QUrl
 from PySide2.QtGui import QCloseEvent, QKeySequence, QIcon
 from PySide2.QtWidgets import (QAction, QApplication, QDesktopWidget,QDockWidget, QLabel, QLineEdit, QMainWindow, QMenu, QInputDialog, QMenuBar, QPushButton,QStatusBar, QToolBar, QTabWidget)
 from PySide2.QtWebEngineWidgets import (QWebEngineDownloadItem, QWebEnginePage, QWebEngineView, QWebEngineProfile)
+#Navigation and load
 class WebLoad(QWebEngineView):
     def __init__(self):
         super(WebLoad,self).__init__()
@@ -18,15 +20,11 @@ class WebLoad(QWebEngineView):
         url = QInputDialog(self)
         value = url.getText(self, "Web URL","URL:",QLineEdit.Normal)
         self.load(QUrl('https://'+(value)[0]))
-        #self.load(QUrl(url))
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle('PyWeb Browser')
         self.webload = WebLoad()
-        #self.qline = QLineEdit(self)
-        #self.qline.setEchoMode(QLineEdit.Password)
-        #self.qline.move(180,2)
         self.toolbar = QToolBar()
         self.exit_gomb = QAction(QIcon("Icons/quit.png"), "Exit")
         self.prev_gomb = QAction(QIcon("Icons/prev.png"), "Back")
@@ -38,7 +36,6 @@ class MainWindow(QMainWindow):
         self.toolbar.addAction(self.next_gomb)
         self.toolbar.addAction(self.reload_gomb)
         self.toolbar.addAction(self.url_gomb)
-        #self.qline.setMinimumSize(800,10)
         self.addToolBar(self.toolbar)
         self.exit_gomb.triggered.connect(self.exit_app)
         self.prev_gomb.triggered.connect(self.webload.prev_page)
@@ -46,7 +43,6 @@ class MainWindow(QMainWindow):
         self.reload_gomb.triggered.connect(self.webload.reload_page)
         self.url_gomb.triggered.connect(self.webload.url_open)
         self.setCentralWidget(self.webload)
-        #self.inputUrl()
     def exit_app(self):
         QApplication.exit()
 def main():
